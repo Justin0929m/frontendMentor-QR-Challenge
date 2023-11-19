447db36724efdf6991c986d853783c3620ca6ac3
Justin0929m <scholtz0929@gmail.com>
2023-11-19 12:07:25 +0200

    qr challenge

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <HelloWorldVue />
+  </div>
+</template>
+
+<script>
+import HelloWorldVue from '@/components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorldVue
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap');
+
+:root{
+  --white: hsl(0, 0%, 100%);
+  --lightGray: hsl(212, 45%, 89%);
+  --grayishBlue: hsl(220, 15%, 55%);
+  --darkBlue: hsl(218, 44%, 22%);
+}
+
+body{
+  background-color: var(--grayishBlue);
+  font-family: 'Outfit', sans-serif;
+  overflow-y: hidden;
+}
+
+p{
+  font-size: 15px;
+  color: var(--grayishBlue);
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <div class="outerBox">
+      <div class="innerBox">
+        <img src="https://i.postimg.cc/bwb3m7fp/image-qr-code.png" class="qr-img" alt="">
+      </div>
+      <div class="para">
+        <h3 class="title">Improve your front-end <br> skills by building projects</h3>
+        <p class="text">Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+img{
+  display: block;
+  max-width: 100%;
+  
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.outerBox {
+  background-color: var(--white);
+  border-radius: 10px;
+  width: 16rem;
+  height: max-content;
+  padding: 1rem;
+}
+
+.innerBox{
+  width: 100%;
+  height: 70%;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.para{
+  text-align: center;
+}
+
+.title{
+  font-weight: 700;
+}
+
+.text{
+  font-weight: 400;
+}
+</style>
